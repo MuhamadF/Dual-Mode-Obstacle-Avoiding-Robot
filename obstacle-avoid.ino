c14d6eb7fdbb0f44253f721212c97a9bbc1338f3
MuhamadF <57528446+MuhamadF@users.noreply.github.com>
2021-09-16 02:05:53 +0700

    added bluetooth integration
    bluetooth integration with SPP-C
    can switch beetween Autonomous Mode and Manual Mode

--- obstacle-avoid.ino
+++ obstacle-avoid.ino
@@ -1,6 +1,7 @@
 #include <NewPing.h>
 
-#define MAX_SPEED 95
+
+#define MAX_SPEED 105
 #define MAX_DIST 100
 
 // PIN SENSOR 1
@@ -25,9 +26,11 @@ int kiriSpeed = 10;  //PWM
 int speedSet = 0;
 int countLeft = 0;
 int countRight = 0;
-
 int jarakL = 0;
 int jarakK = 0;
+
+// VARIABEL BLUETOOTH
+char btData;
 
 boolean goesForward = false;
 
@@ -51,6 +54,11 @@ void setup() {
 
   pinMode(kananSpeed, OUTPUT);
   pinMode(kiriSpeed, OUTPUT);
+
+  // state asli = OBSTACLE AVOID
+  btData = 'A';
+
+  Serial.begin(9600);
 }
 
 void Maju() {
@@ -60,11 +68,13 @@ void Maju() {
     digitalWrite(mundurKiri, LOW);
 
   // experimental
-    for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) {
-      analogWrite(kiriSpeed, speedSet);
-      analogWrite(kananSpeed, speedSet);
-    }
+   // for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) {
+      analogWrite(kiriSpeed, MAX_SPEED);
+      analogWrite(kananSpeed, MAX_SPEED);
+    //}
 }
+
+/*gerakan dasar*/
 
 void Mundur(int d) {
     digitalWrite(majuKanan, LOW);
@@ -73,10 +83,10 @@ void Mundur(int d) {
     digitalWrite(mundurKiri, HIGH);
 
   // experimental
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) {
-    analogWrite(kiriSpeed, speedSet);
-    analogWrite(kananSpeed, speedSet);
-   }
+ // for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=1) {
+    analogWrite(kiriSpeed, MAX_SPEED);
+    analogWrite(kananSpeed, MAX_SPEED);
+   //}
   delay(d);
    
 }
@@ -114,45 +124,97 @@ int readPing(NewPing namaSonar) {
   {
     cm = 1000;
   }
-  Serial.println(cm);
   return cm;
 }
 
-void loop() {
-  jarakK = readPing(sonar1);
-  jarakL = readPing(sonar2);
+void ObstacleAvoidMode() {
+  Berhenti(300);
+  
+  while(1) {
+    
+    if(Serial.available() > 0) {
+    btData = Serial.read();
+    }
+    if(btData == 'M'){
+      ManualMode();
+    }
+    
+    jarakK = readPing(sonar1);
+    jarakL = readPing(sonar2);
     // Kalau stuck di sudut kanan & kiri
     // Mundur, Scan, Belok berdasarkan data sensor.
-    if(countRight == 2 || countLeft == 2) {
-       Berhenti(200);
-       Mundur(400);
+    if(countRight == 3 || countLeft == 3) {
+       Mundur(700);
        jarakK = readPing(sonar1);
        jarakL = readPing(sonar2);
-       Berhenti(200);
+       Berhenti(150);
        if(jarakK < jarakL) {
-          Kiri(320);
+          Kiri(400);
         } else {
-          Kanan(320);
+          Kanan(400);
         }
-        Berhenti(200);
+       Berhenti(90);
        countRight = 0;
        countLeft = 0;
     }
     // Kalau didepan sensor kanan/kiri ada objek
     // belok berdasarkan data dari sensor
-    if(jarakK <= 10 || jarakL <= 10) {
+    if(jarakK <= 5 || jarakL <= 5) {
+       Mundur(200);
        Berhenti(200);
       if(jarakK < jarakL) {
-        Kiri(320);
+        Kiri(400);
         countLeft++;
       } else {
-        Kanan(320);
+        Kanan(400);
         countRight++;
       }
-      Berhenti(300);
+      Berhenti(90);
     } else {
       Maju();
       countLeft = 0;
       countRight = 0;
     }
+  }
+}
+
+void ManualMode() {
+  Berhenti(300);
+  
+  while(1) {
+    if(Serial.available() > 0) {
+    btData = Serial.read();
+    }
+    if(btData == 'A'){
+      ObstacleAvoidMode();
+    }
+     if(btData == 'S') {
+          Berhenti(1);
+     }
+     if(btData == 'F') {
+          Maju();
+     }
+     if(btData == 'B') { 
+          Mundur(1);
+     }
+     if(btData == 'L') {
+          Kiri(1);
+     }
+     if(btData == 'R') {
+          Kanan(1);
+     }
+  }
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    btData = Serial.read();
+  }
+
+  if(btData == 'M'){
+    ManualMode();
+  }
+  if(btData == 'A') {
+    ObstacleAvoidMode();
+  }
 }
